1abe9cba6e08325813be5e310afa64ce6a40fb1a
s.otake <aaa@bbb.ccc>
2023-10-13 21:14:56 +0900

    bgm change

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1697191438|20716386632";
+const CACHE_VERSION = "1697199266|750880493";
 const CACHE_PREFIX = "MandoraKoyo-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
